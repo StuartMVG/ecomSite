3253c0087630c68946c76635a8aa23fb940f68a0
StuartMVG <stuartmvg@gmail.com>
2016-04-25 23:51:15 -0400

    merge changes

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 # ecomSite
 
-<<<<<<< HEAD
+
 Udemy class 3:31
-=======
+
 Backtrack to make sure I didn't miss anything: 2:18
-Udemy class 2:22
->>>>>>> origin/master
 
 ##Installments Needed
 
--- config/secret.js
+++ config/secret.js
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 module.exports = {
 
   database: 'mongodb://root:123abc@ds031972.mongolab.com:31972/ecommerces',
   port: 3000,
   secretKey: "secretKey"
-  
 
-=======
-module.export = {
-  database: 'mongodb://root:123abc@ds031972.mongolab.com:31972/ecommerces',
-  port: 3000,
-  secretKey: 'secretKey'
->>>>>>> origin/master
 }
